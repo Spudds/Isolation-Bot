f999463bff1d0912a5a0acc03bc9b168a3a1fecb
Seb King <spudjking13@outlook.com>
2020-05-27 13:40:13 +0100

    Add announcement command

---
+++ cogs/admin.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def announce(self, ctx, channel: discord.TextChannel, *, arg):
+
+        ch_id = channel.id
+        channel = self.bot.get_channel(ch_id)
+
+        await channel.send(arg)
+        await ctx.send(f"Announcement has been sent to <#{ch_id}>.")
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
